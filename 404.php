895bb06fb64d8fe7b64e74d0ed4e6b2aac5773c0
Philippe Rathé <prathe@gmail.com>
2010-12-23 14:06:32 -0500

    Translate 404

--- 404.php
+++ 404.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 <div class="grid_11">
 	<div id="content">
-		<h2>Error 404 - Page not found</h2>
-		<p>Sorry, the page you are looking for has either been moved or removed from the website.</p>
+		<h2>Erreur 404 - Page introuvable</h2>
+		<p>Désolé, la page demandée a été effacée ou déplacée.</p>
 	</div>
 </div>
 
