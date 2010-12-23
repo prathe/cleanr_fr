b5843d266606c32aa856c9c0f3bb61756d0a3d9b
Philippe Rathé <prathe@gmail.com>
2010-12-23 14:22:48 -0500

    Translate sidebar

--- sidebar.php
+++ sidebar.php
@@ -21,26 +21,22 @@
 
 			<?php /* If this is a 404 page */ if (is_404()) { ?>
 			<?php /* If this is a category archive */ } elseif (is_category()) { ?>
-			<p>You are currently browsing the archives for the <?php single_cat_title(''); ?> category.</p>
+			<p>Archives de la catégorie <strong><?php single_cat_title(''); ?></strong></p>
 
 			<?php /* If this is a yearly archive */ } elseif (is_day()) { ?>
-			<p>You are currently browsing the <a href="<?php bloginfo('url'); ?>/"><?php echo bloginfo('name'); ?></a> blog archives
-			for the day <?php the_time('l, F jS, Y'); ?>.</p>
+			<p>Archives du <?php the_time('j F Y'); ?>.</p>
 
 			<?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
-			<p>You are currently browsing the <a href="<?php bloginfo('url'); ?>/"><?php echo bloginfo('name'); ?></a> blog archives
-			for <?php the_time('F, Y'); ?>.</p>
+			<p>Archives de <?php the_time('F Y'); ?>.</p>
 
 			<?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
-			<p>You are currently browsing the <a href="<?php bloginfo('url'); ?>/"><?php echo bloginfo('name'); ?></a> blog archives
-			for the year <?php the_time('Y'); ?>.</p>
+			<p>Archives de <?php the_time('Y'); ?>.</p>
 
-			<?php /* If this is a monthly archive */ } elseif (is_search()) { ?>
-			<p>You have searched the <a href="<?php echo bloginfo('url'); ?>/"><?php echo bloginfo('name'); ?></a> blog archives
-			for <strong>'<?php the_search_query(); ?>'</strong>. If you are unable to find anything in these search results, you can try one of these links.</p>
+			<?php /* If this is a search archive */ } elseif (is_search()) { ?>
+			<p>Résultats pour <strong>'<?php the_search_query(); ?>'</strong>.</p>
 
-			<?php /* If this is a monthly archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
-			<p>You are currently browsing the <a href="<?php echo bloginfo('url'); ?>/"><?php echo bloginfo('name'); ?></a> blog archives.</p>
+			<?php /* If this is a paged archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
+			<p>Page <?php $_GET['paged'] ?> des archives.</p>
 
 			<?php } ?>
 
@@ -54,21 +50,10 @@
 				</ul>
 			</li>
 
-			<?php wp_list_categories('title_li=<h2>Categories</h2>'); ?>
+			<?php wp_list_categories('title_li=<h2>Catégories</h2>'); ?>
 
 			<?php /* If this is the frontpage */ if ( is_home() || is_page() ) { ?>
 				<?php wp_list_bookmarks(); ?>
-
-				<li><h2>Meta</h2>
-				<ul>
-					<?php wp_register(); ?>
-					<li><?php wp_loginout(); ?></li>
-					<li><a href="http://validator.w3.org/check/referer" title="This page validates as XHTML 1.0 Transitional">Valid <abbr title="eXtensible HyperText Markup Language">XHTML</abbr></a></li>
-					<li><a href="http://gmpg.org/xfn/"><abbr title="XHTML Friends Network">XFN</abbr></a></li>
-					<li><a href="http://wordpress.org/" title="Powered by WordPress, state-of-the-art semantic personal publishing platform.">WordPress</a></li>
-					<?php wp_meta(); ?>
-				</ul>
-				</li>
 			<?php } ?>
 
 			<?php endif; ?>
