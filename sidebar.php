e3c25fadd0787f2d42bc7e167b8b91b434cab20a
Philippe Rathé <prathe@gmail.com>
2010-12-23 18:26:36 -0500

    Remove useless current navigation title in sidebar.

--- sidebar.php
+++ sidebar.php
@@ -15,33 +15,6 @@
 			<p>A little something about you, the author. Nothing lengthy, just an overview.</p>
 			</li>-->
 			
-			<?php if ( is_404() || is_category() || is_day() || is_month() ||
-						is_year() || is_search() || is_paged() ) {
-			?> <li>
-
-			<?php /* If this is a 404 page */ if (is_404()) { ?>
-			<?php /* If this is a category archive */ } elseif (is_category()) { ?>
-			<p>Archives de la catégorie <strong><?php single_cat_title(''); ?></strong></p>
-
-			<?php /* If this is a yearly archive */ } elseif (is_day()) { ?>
-			<p>Archives du <?php the_time('j F Y'); ?>.</p>
-
-			<?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
-			<p>Archives de <?php the_time('F Y'); ?>.</p>
-
-			<?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
-			<p>Archives de <?php the_time('Y'); ?>.</p>
-
-			<?php /* If this is a search archive */ } elseif (is_search()) { ?>
-			<p>Résultats pour <strong>'<?php the_search_query(); ?>'</strong>.</p>
-
-			<?php /* If this is a paged archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
-			<p>Page <?php $_GET['paged'] ?> des archives.</p>
-
-			<?php } ?>
-
-			</li> <?php }?>
-
 			<?php //wp_list_pages('title_li=<h2>Pages</h2>' ); ?>
 
 			<li><h2>Archives</h2>
