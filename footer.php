d368a5cf220ca41752090865dfc84fc2ec56f403
Philippe Rathé <prathe@gmail.com>
2010-12-23 23:40:25 -0500

    Add mailto

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
 <hr />
 <div id="footer" class="grid_16">
 	<p>
-		&copy; <?php echo date('Y'); ?>. <?php bloginfo('name'); ?>. Thème <a href="http://wordpress.org/">WordPress</a> par <a href="http://wpshoppe.com/">WPShoppe</a>.
+		&copy; <?php echo date('Y'); ?>. <?php bloginfo('name'); ?>. Thème <a href="http://wordpress.org/">WordPress</a> par <a href="http://wpshoppe.com/">WPShoppe</a>.<br />
+		<a href="mailto:info@arnela.ca">info@arnela.ca</a>
 	</p>
 </div>
 </div>
