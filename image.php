3c1dcce76fb9d126f3f97a6b65c19e330ca51a40
Philippe Rathé <prathe@gmail.com>
2011-03-23 21:41:16 -0400

    Remove verbose post meta data

--- image.php
+++ image.php
@@ -20,29 +20,8 @@
 
 				<p class="postmetadata alt">
 					<small>
-						This entry was posted on <?php the_time('l, F jS, Y') ?> at <?php the_time() ?>
-						and is filed under <?php the_category(', ') ?>.
-						<?php the_taxonomies(); ?>
-						You can follow any responses to this entry through the <?php post_comments_feed_link('RSS 2.0'); ?> feed.
-
-						<?php if (('open' == $post-> comment_status) && ('open' == $post->ping_status)) {
-							// Both Comments and Pings are open ?>
-							You can <a href="#respond">leave a response</a>, or <a href="<?php trackback_url(); ?>" rel="trackback">trackback</a> from your own site.
-
-						<?php } elseif (!('open' == $post-> comment_status) && ('open' == $post->ping_status)) {
-							// Only Pings are Open ?>
-							Responses are currently closed, but you can <a href="<?php trackback_url(); ?> " rel="trackback">trackback</a> from your own site.
-
-						<?php } elseif (('open' == $post-> comment_status) && !('open' == $post->ping_status)) {
-							// Comments are open, Pings are not ?>
-							You can skip to the end and leave a response. Pinging is currently not allowed.
-
-						<?php } elseif (!('open' == $post-> comment_status) && !('open' == $post->ping_status)) {
-							// Neither Comments, nor Pings are open ?>
-							Both comments and pings are currently closed.
-
-						<?php } edit_post_link('Edit this entry.','',''); ?>
-
+						<?php the_time('j F Y') ?>, <?php the_category(', ') ?>.
+						<?php edit_post_link('Modifier cet article','','.'); ?>
 					</small>
 				</p>
 
--- single.php
+++ single.php
@@ -16,28 +16,8 @@
 
 				<p class="postmetadata alt">
 					<small>
-						Cet article a été publié le <?php the_time('j F Y') ?> à <?php the_time() ?>
-						sous <?php the_category(', ') ?>.
-						Vous pouvez vous abonner aux <?php post_comments_feed_link('fil RSS'); ?> des commentaires de cet article.
-
-						<?php if (('open' == $post-> comment_status) && ('open' == $post->ping_status)) {
-							// Both Comments and Pings are open ?>
-							Vous pouvez <a href="#respond">laisser un commentaire</a> ou faîtes un <a href="<?php trackback_url(); ?>" rel="trackback">rétrolien</a> depuis votre site.
-
-						<?php } elseif (!('open' == $post-> comment_status) && ('open' == $post->ping_status)) {
-							// Only Pings are Open ?>
-							Vous pouvez faire un <a href="<?php trackback_url(); ?> " rel="trackback">rétrolien</a> depuis votre site.
-
-						<?php } elseif (('open' == $post-> comment_status) && !('open' == $post->ping_status)) {
-							// Comments are open, Pings are not ?>
-							Vous pouvez laisser un commentaire.
-
-						<?php } elseif (!('open' == $post-> comment_status) && !('open' == $post->ping_status)) {
-							// Neither Comments, nor Pings are open ?>
-							La section des commentaires est fermée.
-
-						<?php } edit_post_link('Modifier cet article','','.'); ?>
-
+						<?php the_time('j F Y') ?>, <?php the_category(', ') ?>.
+						<?php edit_post_link('Modifier cet article','','.'); ?>
 					</small>
 				</p>
 
